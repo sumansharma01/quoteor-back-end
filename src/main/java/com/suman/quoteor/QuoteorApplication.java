829acd807fdef4bd9add5356980cfd5245151f3d
sumansharma01 <suman.sharma0331@gmail.com>
2021-03-24 15:16:17 +0530

    initialized the project

---
+++ src/main/java/com/suman/quoteor/QuoteorApplication.java
@@ -0,0 +1,13 @@
+package com.suman.quoteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suman/quoteor/QuoteorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suman.quoteor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
